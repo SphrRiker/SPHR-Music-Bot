b14f901217ccb04ec830dcf0e8cc028aea2e7c83
SphrRiker <59436254+SphrRiker@users.noreply.github.com>
2020-10-24 00:59:19 +0330

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,28 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "help",
+  aliases: ["h"],
+  description: "Display all commands and descriptions",
+  execute(message) {
+    let commands = message.client.commands.array();
+
+    let helpEmbed = new MessageEmbed()
+      .setTitle("SPHR's Bot")
+      .setDescription("List of all commands")
+      .setColor("#F8AA2A")
+      .setFooter('👨🏾‍💻 Bot Developer : SPHR#1000');
+
+    commands.forEach((cmd) => {
+      helpEmbed.addField(
+        `**${message.client.prefix}${cmd.name} ${cmd.aliases ? `(${cmd.aliases})` : ""}**`,
+        `${cmd.description}`,
+        true
+      );
+    });
+
+    helpEmbed.setTimestamp();
+
+    return message.channel.send(helpEmbed).catch(console.error);
+  }
+};
